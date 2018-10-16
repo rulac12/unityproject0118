06e16eddf3c5071830e93403d3263d22195fa2d4
Michaelle lopez <mlopez-rodriguez1@csub.edu>
2018-10-16 13:10:04 -0700

    added a menu screen

---
+++ New Unity Project/Assets/C# Scripts and Music/PauseMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    public GameObject FPSController;
+
+    private void Start()
+    {
+        FPSController = GameObject.FindWithTag("Player");
+    }
+    
+
+
+    // Update is called once per frame
+    void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                
+                }
+                else
+                {
+                    Pause();
+                
+            }
+            }
+        }
+
+ 
+    public void Resume()
+        {
+            PauseMenuUI.SetActive(false);
+            FPSController.GetComponent<FirstPersonController>().enabled = true;
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+         }
+
+        void Pause()
+        {
+            PauseMenuUI.SetActive(true);
+            FPSController.GetComponent<FirstPersonController>().enabled = false;
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+            
+         }
+
+        public void LoadMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+    }
